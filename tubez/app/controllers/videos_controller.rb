f7f0593e55f8fda5ab55593bef8d021ea6a26ffa
Chyld Medford <chyld.medford@gmail.com>
2013-02-23 12:47:21 -0500

    final

--- tubez/app/controllers/videos_controller.rb
+++ tubez/app/controllers/videos_controller.rb
@@ -1,4 +1,23 @@
+require 'open-uri'
+
 class VideosController < ApplicationController
+  def index
+    @videos = Video.all
+  end
   def new
+  end
+  def search
+    search = params[:video]
+    url = "http://youtube.com/results?search_query=#{search}"
+    html = Nokogiri::HTML(open(url))
+    lis = html.css('#search-results li')
+    lis.each do |li|
+      url = li.get_attribute('data-context-item-id')
+      # views = li.get_attribute('data-context-item-views')
+      title = li.get_attribute('data-context-item-title')
+      Video.create(:title => title, :url => url)
+    end
+
+    redirect_to(videos_path)
   end
 end
