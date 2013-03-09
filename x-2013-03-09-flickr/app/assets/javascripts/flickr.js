2526768f5d3eb5818d3719e25782d83fc1041d4d
Chyld Medford <chyld.medford@gmail.com>
2013-03-09 09:57:24 -0500

    flickr

--- x-2013-03-09-flickr/app/assets/javascripts/flickr.js
+++ x-2013-03-09-flickr/app/assets/javascripts/flickr.js
@@ -7,5 +7,12 @@ function start_program()
 
 function search_flickr()
 {
-  alert('i am searching...');
+  var page = 1;
+  var query = $('input').val();
+  $.getJSON('http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=4c6e1f137bbeb666a1c0a884b609e286&text=' + query + '&per_page=500&page=' + page + '&format=json&jsoncallback=?', flickr_finished);
+}
+
+function flickr_finished(data)
+{
+
 }
